04cb297f0bba6c9f0d6b8ad7b16530fb5668c322
benjaminhernandez1981 <benjamineric2005@gmail.com>
2024-07-12 22:49:00 -0500

    wk5-library-react-tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Featured from "./components/Featured";
+import Highlights from "./components/Highlights";
+import Landing from "./components/Landing";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
     </div>
   );
 }
